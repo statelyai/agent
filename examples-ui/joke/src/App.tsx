f60571941387e460388df18dc90cccf77be31bda
Kevin Maes <kevin@kevinmaes.com>
2024-02-20 19:16:30 +0100

    Delete Vite svg

--- examples-ui/joke/src/App.tsx
+++ examples-ui/joke/src/App.tsx
@@ -2,18 +2,12 @@ import { useState } from 'react';
 import './App.css';
 import { Joke } from './Joke';
 import { TicTacToe } from './TicTacToe';
-import viteLogo from '/vite.svg';
 
 function App() {
   const [exampleId, setExampleId] = useState<string | null>(null);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
       <h1>Stately Agent Examples</h1>
       <button onClick={() => setExampleId('joke')}>Joke</button>
       <button onClick={() => setExampleId('tic-tac-toe')}>Tic Tac Toe</button>
