2b42562edd1a1f127bcb707a0b6a7286db7b19e8
Kevin Maes <kevin@kevinmaes.com>
2024-02-20 22:16:44 +0100

    Reduce the joke example UI to only run the joke example

--- examples-ui/joke/server/index.ts
+++ examples-ui/joke/server/index.ts
@@ -3,7 +3,6 @@ import * as express from 'express';
 import * as cors from 'cors';
 
 import { createJokeMachine } from './examples/joke';
-import { createTicTacToeAgent } from './examples/ticTacToe';
 
 dotenv.config();
 
@@ -50,33 +49,6 @@ app.post('/joke-set-topic', (req, res) => {
   jokeAgent.send({ type: 'setTopic', topic });
 
   res.send(200);
-});
-
-app.get('/tic-tac-toe', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  // Function to send a message
-  const sendEvent = (data) => {
-    const message = `data: ${JSON.stringify(data)}\n\n`;
-    res.write(message);
-  };
-
-  sendEvent({ message: 'Connection established' });
-
-  const ticTacToeAgent = createTicTacToeAgent();
-
-  // Clean up when the connection is closed
-  req.on('close', () => {
-    ticTacToeAgent.stop();
-    res.end();
-  });
-
-  ticTacToeAgent.subscribe((state) => {
-    sendEvent({ message: state });
-  });
-  ticTacToeAgent.start();
 });
 
 app.listen(port, () => {
--- examples-ui/joke/src/App.tsx
+++ examples-ui/joke/src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from 'react';
 import './App.css';
 import { Joke } from './Joke';
-import { TicTacToe } from './TicTacToe';
 
 function App() {
-  const [exampleId, setExampleId] = useState<string | null>(null);
-
   return (
     <>
       <h1>Stately Agent Examples</h1>
-      <button onClick={() => setExampleId('joke')}>Joke</button>
-      <button onClick={() => setExampleId('tic-tac-toe')}>Tic Tac Toe</button>
-      {exampleId && exampleId === 'joke' ? (
-        <Joke />
-      ) : exampleId === 'tic-tac-toe' ? (
-        <TicTacToe />
-      ) : null}
+      <Joke />
     </>
   );
 }
