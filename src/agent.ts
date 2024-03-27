2d5047a7051f3338d2e89fecc812406852dac4f9
David Khourshid <davidkpiano@gmail.com>
2024-03-28 00:34:27 +0900

    WIP

--- src/agent.ts
+++ src/agent.ts
@@ -1,8 +1,87 @@
-import { AnyStateMachine, createActor } from 'xstate';
+import { inspect } from 'util';
+import {
+  ActorOptions,
+  AnyActorLogic,
+  AnyActorRef,
+  AnyEventObject,
+  AnyStateMachine,
+  EventObject,
+  InputFrom,
+  createActor,
+} from 'xstate';
 
-export function createAgent<T extends AnyStateMachine>(
-  ...args: Parameters<typeof createActor<T>>
+export type AgentExperiences<T, R> = Record<
+  string,
+  Record<
+    string,
+    {
+      state: T;
+      reward: R;
+    }
+  >
+>;
+
+export type AgentPlan<T> = Array<{
+  /**
+   * The current state
+   */
+  state: T;
+  /**
+   * The event to execute
+   */
+  event: AnyEventObject;
+}>;
+
+export interface AgentModel<T, R> {
+  experiences: AgentExperiences<T, any>;
+  getMachine: () => AnyStateMachine;
+  policy: (state: T, goal: T) => Promise<AgentPlan<T>>;
+  getNextEvents: (state: T) => Promise<AnyEventObject[]>;
+  getPlans: (state: T, goal: T) => Promise<Array<AgentPlan<T>>>;
+  getReward: (state: T, goal: T, action: EventObject) => Promise<R>;
+}
+
+export interface AgentLogic<T> {
+  /**
+   * The next possible actions (represented by events) that the agent can take
+   * based on the current state of the environment
+   */
+  getActions(state: T): Promise<AnyEventObject[]>;
+  getPlan(state: T, goal: any): Promise<Array<[T, EventObject]>>;
+}
+
+export interface Agent<T extends AnyActorRef> {
+  experiences: Array<{
+    currentState: T;
+    event: AnyEventObject;
+    nextState: T;
+    // plan (event array)
+    // reason (string)
+  }>;
+  act(environment: T): Promise<any>;
+}
+
+export function createAgent<TLogic extends AnyActorLogic>(
+  goal: string,
+  logic: TLogic,
+  input: InputFrom<TLogic>
 ) {
-  const [machine, options] = args;
-  return createActor(machine, options);
+  const experiences: Agent<any>['experiences'] = [];
+
+  const actor = createActor(logic, {
+    input,
+    inspect: (inspEv) => {
+      if (inspEv.type === '@xstate.snapshot') {
+        experiences.push({
+          nextState: inspEv.state.value,
+          event: inspEv.event,
+        });
+      }
+    },
+  });
+
+  return {
+    experiences,
+    ...actor,
+  };
 }
